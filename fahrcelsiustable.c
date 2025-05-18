28a4f140ad38b7baa83195d13cae598e2494ce0e
Karl S <lillerik.sund@gmail.com>
2025-05-18 20:40:28 +0200

    Initial Commit

---
+++ fahrcelsiustable.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/* print fahrenheit-celsius table
+ * for fahr = 0, 20, ..., 300 */
+
+main()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+	
+	lower = 0;	/* lower limit of temperature table*/
+	upper = 300;	/* upper limit */
+	step = 20;	/* step size */
+
+
+	fahr = lower;
+	printf("Fahrenheit - C\n");
+	while(fahr <= upper) {
+		celsius = 5.0/9.0 * (fahr-32.0);
+		printf("%3.0f\t  %6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}
