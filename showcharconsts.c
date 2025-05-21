950c37bfd10a195a57a2cd867854fa1354225831
karlsune <you@example.com>
2025-05-21 08:28:12 +0200

    Developed solution to K&R C Exercise 1-10

--- showcharconsts.c
+++ showcharconsts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /* A program that copies its input to output, replacing each tab by \t, each backspace by \b,
- *
+ */
 
 int main()
 {
@@ -11,4 +11,27 @@ int main()
 	int backslash = '\\';
 	
 	while ((c = getchar()) != EOF)
-		...
+	{
+		if (c == tab)
+		{
+			putchar('\\');
+			putchar('t');
+			continue;
+		}
+		if (c == backspace)
+		{
+			putchar('\\');
+			putchar('b');
+			continue;
+		}
+		if (c == backslash) 
+		{
+			putchar('\\');
+			putchar('\\');
+			continue;
+		}
+
+		putchar(c);
+			
+	}
+}
